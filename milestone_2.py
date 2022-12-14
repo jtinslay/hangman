c4605c1f28d05e583bca765a96e9541e6482db9b
Jane Tinslay <jtinslay@Janes-MBP.broadband>
2022-12-14 15:57:30 +0000

    Adding text to ask user to input a letter from the alphabet

--- milestone_2.py
+++ milestone_2.py
@@ -1,16 +1,17 @@
 import random
 
 word_list = ["Apples", "Oranges", "Strawberries", "Blueberries", "Kiwis"]
-print(word_list)
 
 word = random.choice(word_list)
 
 print(word)
 
+print("Please enter a letter from the alphabet:")
 guess = input()
 
 if len(guess) == 1 and guess.isalpha():
     print("Good guess!")
 else:
     print("Oops! That is not a valid input.")
-    5
+    
+
