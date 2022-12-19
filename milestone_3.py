32c9a5b94d6c3cb710a1e0621687df882a048f59
Jane Tinslay <jtinslay@Janes-MBP.broadband>
2022-12-19 12:51:17 +0000

    Checks if guess is in the word. Case insensitive

--- milestone_3.py
+++ milestone_3.py
@@ -1,8 +1,17 @@
+import random
+
+word_list = ["Apples", "Oranges", "Strawberries", "Blueberries", "Kiwis"]
+
+word = random.choice(word_list)
+print(word)
 
 while True:
     print("Type a letter: ")
     guess = input()
     if len(guess) == 1 and guess.isalpha():
-        break
+        if guess.lower() in word.lower():
+             print(f"Good guess! {guess} is in the word")
+        else:
+             print(f"Sorry, {guess} is not in the word. Try again")
     else:
         print("Please, enter a single alphabetical character.")
