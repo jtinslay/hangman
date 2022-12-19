ccc04dcf60a09c337c9a8effa15efce3bbe94f44
Jane Tinslay <jtinslay@Janes-MBP.broadband>
2022-12-19 13:08:11 +0000

    Refactor into functions

--- milestone_3.py
+++ milestone_3.py
@@ -2,16 +2,25 @@ import random
 
 word_list = ["Apples", "Oranges", "Strawberries", "Blueberries", "Kiwis"]
 
-word = random.choice(word_list)
+word = random.choice(word_list).lower()
 print(word)
 
-while True:
-    print("Type a letter: ")
-    guess = input()
-    if len(guess) == 1 and guess.isalpha():
-        if guess.lower() in word.lower():
-             print(f"Good guess! {guess} is in the word")
-        else:
-             print(f"Sorry, {guess} is not in the word. Try again")
+def check_guess(guess):
+    if guess.lower() in word:
+        print(f"Good guess! {guess} is in the word")
     else:
-        print("Please, enter a single alphabetical character.")
+        print(f"Sorry, {guess} is not in the word. Try again")
+
+def ask_for_input():
+    while True:
+        print("Type a letter: ")
+        guess = input()
+
+        if len(guess) == 1 and guess.isalpha():
+            break
+        else:
+             print("Please, enter a single alphabetical character.")
+
+    check_guess(guess)
+
+ask_for_input()
