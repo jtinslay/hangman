03c53bee49a0b7e7c36514ac072076a481fb7249
Jane Tinslay <jtinslay@Janes-MBP.broadband>
2022-12-21 11:22:19 +0000

    Removing transforming random choice word to lower case

--- milestone_4.py
+++ milestone_4.py
@@ -4,7 +4,7 @@ class Hangman:
     def __init__(self, word_list, num_lives = 5):
         self.word_list = word_list
         self.num_lives = num_lives
-        self.word = random.choice(word_list).lower()
+        self.word = random.choice(word_list)
 
         self.word_guessed = ["_" for character in self.word]
 
