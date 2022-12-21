ffbb5670b1f6a82d97046fb1f4a63cb5e808fee1
Jane Tinslay <jtinslay@Janes-MBP.broadband>
2022-12-21 02:07:22 +0000

    Create Hangman class

---
+++ milestone_4.py
@@ -0,0 +1,21 @@
+import random
+
+class Hangman:
+    def __init__(self, word_list, num_lives = 5):
+        self.word_list = word_list
+        self.num_lives = num_lives
+        self.word = random.choice(word_list).lower()
+
+        self.word_guessed = ["_" for character in self.word]
+
+        self.num_letters = len(set(word_list))
+        self.list_of_guesses = []
+
+
+word_list = ["Apples", "Oranges", "Strawberries", "Blueberries", "Kiwis"]
+
+hangman = Hangman(word_list)
+print(hangman.word)
+print(hangman.word_guessed)
+print(hangman.num_letters)
+
