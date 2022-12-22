6c0c878ff368c6a91badc3cd4df19df8ec74bf37
Jane Tinslay <jtinslay@Janes-MBP.broadband>
2022-12-22 10:37:58 +0000

    Cleanup print statements

--- milestone_4.py
+++ milestone_4.py
@@ -7,7 +7,6 @@ class Hangman:
 
         self.word = random.choice(word_list)
         self.word_guessed = ["_" for _ in range(len(self.word))]
-
         self.num_letters = len(set(self.word))
         self.list_of_guesses = []
 
@@ -18,33 +17,26 @@ class Hangman:
             print(f"Good guess! {guess} is in the word.")
 
     def ask_for_input(self):
-
+        print(self.word)
+        
         while True:
             print("Guess a letter!")
-
             guess = input()
-            print(f"guess {guess}")
 
             if not len(guess) == 1 or not guess.isalpha():
                 print("Invalid Letter. Please, enter a single alphabetical character.")
 
             elif guess in self.list_of_guesses:
-                    print("You already tried that letter!")
+                print("You already tried that letter!")
 
             else:
                 self.check_guess(guess)
-                print(f"guess {guess}")
 
                 self.list_of_guesses.append(guess)
-                print(self.list_of_guesses)
-
 
 
 word_list = ["Apples", "Oranges", "Strawberries", "Blueberries", "Kiwis"]
 
 hangman = Hangman(word_list)
-print(hangman.word)
-print(hangman.word_guessed)
-print(hangman.num_letters)
 
-#hangman.ask_for_input()
+hangman.ask_for_input()
